df3c5d198abd5691051114c502ac6116effbb189
mhfavila <61873992+mhfavila@users.noreply.github.com>
2021-03-16 10:21:28 +0100

    Clases

---
+++ EE_PR_MonteroFavila/src/ClasesDTO/PersonajeDTO.java
@@ -0,0 +1,95 @@
+package ClasesDTO;
+
+public class PersonajeDTO {
+	private String Nombre;
+	private int Nivel;
+	private int Vida;
+	private int Fuerza;
+	private int Destreza;
+	private int Constitucion;
+	private int Oro;
+	private String tipoPersonaje;
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public int getNivel() {
+		return Nivel;
+	}
+
+	public void setNivel(int nivel) {
+		Nivel = nivel;
+	}
+
+	public int getVida() {
+		return Vida;
+	}
+
+	public void setVida(int vida) {
+		Vida = vida;
+	}
+
+	public int getFuerza() {
+		return Fuerza;
+	}
+
+	public void setFuerza(int fuerza) {
+		Fuerza = fuerza;
+	}
+
+	public int getDestreza() {
+		return Destreza;
+	}
+
+	public void setDestreza(int destreza) {
+		Destreza = destreza;
+	}
+
+	public int getConstitucion() {
+		return Constitucion;
+	}
+
+	public void setConstitucion(int constitucion) {
+		Constitucion = constitucion;
+	}
+
+	public int getOro() {
+		return Oro;
+	}
+
+	public void setOro(int oro) {
+		Oro = oro;
+	}
+
+	public String getTipoPersonaje() {
+		return tipoPersonaje;
+	}
+
+	public void setTipoPersonaje(String tipoPersonaje) {
+		this.tipoPersonaje = tipoPersonaje;
+	}
+
+	public PersonajeDTO(String Nombre, int Nivel, int Vida, int Fuerza, int Destreza, int Constitucion, int Oro,
+			String tipoPersonaje) {
+		this.Nombre = Nombre;
+		this.Nivel = Nivel;
+		this.Vida = Vida;
+		this.Fuerza = Fuerza;
+		this.Destreza = Destreza;
+		this.Constitucion = Constitucion;
+		this.Oro = Oro;
+		this.tipoPersonaje = tipoPersonaje;
+	}
+
+	@Override
+	public String toString() {
+		return this.Nombre + " " + this.Nivel + " " + this.Vida + " " + this.Fuerza + " " + this.Fuerza + " "
+				+ this.Destreza + " " + this.Constitucion + " " + this.Oro + " " + this.tipoPersonaje;
+	}
+
+}
