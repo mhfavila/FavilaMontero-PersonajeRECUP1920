3e2bb4214abb0298ebce84fe9ab1ec7d27c447c2
mhfavila <61873992+mhfavila@users.noreply.github.com>
2021-03-21 16:59:24 +0100

    sa

--- EE_PR_MonteroFavila/src/GestionPersonaje.java
+++ EE_PR_MonteroFavila/src/GestionPersonaje.java
@@ -1,22 +1,54 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 import ClasesDTO.PersonajeDTO;
 
-
 public class GestionPersonaje {
+	Scanner sc = new Scanner(System.in);
+	ArrayList<PersonajeDTO> listaPersonajes = new ArrayList<>();
 
-	ArrayList <PersonajeDTO> listaPersonajes = new ArrayList<>();
-	
 	public ArrayList<PersonajeDTO> getListaPersonajes() {
 		return listaPersonajes;
 	}
+
 	public void setListaPersonajes(ArrayList<PersonajeDTO> listaPersonajes) {
 		this.listaPersonajes = listaPersonajes;
 	}
-	//preguntar para que uso esto
-	
-	
-	public void añadirPersonaje() {
+	// preguntar para que uso esto
+
+	/**
+	 * añadimos un nuevo personaje
+	 */
+	public void nuevoPersonaje() {
+		Scanner sc = new Scanner(System.in);
+
+		PersonajeDTO personaje = new PersonajeDTO();
+
+		System.out.print("Nombre: ");
+		personaje.setNombre(sc.nextLine());
+		System.out.print("Nivel: ");
+		personaje.setNivel(sc.nextInt());
+		System.out.print("Vida: ");
+		personaje.setVida(sc.nextInt());
+		System.out.print("Fuerza: ");
+		personaje.setFuerza(sc.nextInt());
+		System.out.print("Destreza: ");
+		personaje.setDestreza(sc.nextInt());
+		System.out.print("Constitucion: ");
+		personaje.setConstitucion(sc.nextInt());
+		System.out.print("Inteligencia: ");
+		personaje.setInteligencia(sc.nextInt());
+		System.out.print("Oro: ");
+		personaje.setOro(sc.nextInt());
+
+		listaPersonajes.add(personaje);
+
+	}
+	/**
+	 * cargamos los personajes que ya tenemos creados
+	 */
+	public void cargarPersonaje() {
 		PersonajeDTO personaje = new PersonajeDTO();
 		personaje.setNombre("goku");
 		personaje.setNivel(10);
@@ -26,18 +58,151 @@ public class GestionPersonaje {
 		personaje.setConstitucion(50);
 		personaje.setInteligencia(50);
 		personaje.setOro(50);
-		
-		//Nose si aqui hay que poner el tipo de personaje
-		
+
+		// Nose si aqui hay que poner el tipo de personaje
+
 		listaPersonajes.add(personaje);
-		
+		PersonajeDTO personaje1 = new PersonajeDTO();
+		personaje1.setNombre("DAN");
+		personaje1.setNivel(100);
+		personaje1.setVida(100);
+		personaje1.setFuerza(70);
+		personaje1.setDestreza(30);
+		personaje1.setConstitucion(20);
+		personaje1.setInteligencia(50);
+		personaje1.setOro(90);
+
+		// Nose si aqui hay que poner el tipo de personaje
+
+		listaPersonajes.add(personaje1);
+
 	}
+
+	/**
+	 * para mostrar la lista de personajes
+	 */
 	public void mostrarListaPesonajes() {
-		for(int i=0;i<listaPersonajes.size();i++) {
-			PersonajeDTO aux=listaPersonajes.get(i);
+		System.out.println("Nombre|Nivel|Vida|Fuerza|Destreza|Constitucion|Inteligencia|Oro");
+		for (int i = 0; i < listaPersonajes.size(); i++) {
+			PersonajeDTO aux = listaPersonajes.get(i);
 			System.out.println(aux.toString());
-			System.out.println(aux.getNombre());
+			//System.out.println(aux.getNombre());
 		}
 	}
 	
+	/**
+	 * Buscaremos el nombre del personaje el cual le pasaremos como parametro
+	 * @param Nombre
+	 * @return
+	 */
+	public PersonajeDTO Buscar(String Nombre) {
+		for(int i=0;i<listaPersonajes.size();i++) {
+			PersonajeDTO aux=listaPersonajes.get(i);
+			if(Nombre==aux.getNombre()) {
+				return aux;
+			}
+		}
+		return null;
+		
+
+	}
+	//prueba para modificar datos de un personaje
+	public void BuscarInfo(String Nombre) {
+		for(int i=0;i<listaPersonajes.size();i++) {
+			PersonajeDTO aux=listaPersonajes.get(i);
+			if(Nombre==aux.getNombre()) {
+				System.out.println("Nombre|Nivel|Vida|Fuerza|Destreza|Constitucion|Inteligencia|Oro");
+				System.out.println(aux.toString());
+				
+			}else {
+				System.out.println("No se ha encontrado el nombre");
+			}
+		}
+		
+		
+
+	}
+	/**
+	 * sacamos la informacion de un personaje segun su nombre que le hemos pasado
+	 * @param Nombre
+	 */
+	public void informacionPersonaje(String Nombre){
+		System.out.println("Nombre|Nivel|Vida|Fuerza|Destreza|Constitucion|Inteligencia|Oro");
+		PersonajeDTO aux=Buscar(Nombre);
+		System.out.println(aux.toString());
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public void modificarDatos(String Nombre,int Nivel,int Vida,int Fuerza,int Destreza,int Constitucion,int Inteligencia,int Oro) {
+		
+		for(int i=0;i<listaPersonajes.size();i++) {
+			PersonajeDTO aux=listaPersonajes.get(i);
+			if(Nombre==aux.getNombre()) {
+				
+				aux.setNivel(Nivel);
+				aux.setVida(Vida);
+				aux.setFuerza(Fuerza);
+				aux.setDestreza(Destreza);
+				aux.setConstitucion(Constitucion);
+				aux.setInteligencia(Inteligencia);
+				aux.setOro(Oro);
+				System.out.println(aux.toString());
+				mostrarListaPesonajes();
+			
+			}
+		}
+		
+	}
+	
+	public void modificarPersonaje(String Nombre) {
+		Boolean modificado = false;
+		for(int i=0;i<listaPersonajes.size();i++) {
+			PersonajeDTO aux=Buscar(Nombre);
+			if(Nombre==aux.getNombre()) {
+				System.out.println("Nombre|Nivel|Vida|Fuerza|Destreza|Constitucion|Inteligencia|Oro");
+				System.out.println(aux.toString());
+				
+			
+			
+				do {
+	
+					System.out.println("1. Modificar Nivel");
+					System.out.println("2. Modificar Vida");
+					
+					switch(sc.nextInt()) {
+						case 1:
+							System.out.print("Nueva Nivel: ");
+							int nivel=sc.nextInt();
+							listaPersonajes.get(i).setNivel(nivel);
+							break;
+						case 2:
+							System.out.print("Nuevo Vida: ");
+							int vida=sc.nextInt();
+							listaPersonajes.get(i).setVida(vida);
+							break;
+					}
+					
+					modificado=true;
+					System.out.print("Has terminado? (S/N):");
+					sc.nextLine();					
+				}while(sc.nextLine().equalsIgnoreCase("N"));
+				break;
+			}
+		}
+		if(!modificado) {
+			System.out.println("Personaje no encontrado");
+		}
+	}
+	
+	
+	
+	
 }
+
--- EE_PR_MonteroFavila/src/Principal.java
+++ EE_PR_MonteroFavila/src/Principal.java
@@ -1,14 +1,80 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+import ClasesDTO.PersonajeDTO;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		ArrayList<PersonajeDTO> listaPersonajes = new ArrayList<>();
 		GestionPersonaje gp = new GestionPersonaje();
+
+		// estan bien hechos ya los siguientes
+		/*
+		 * gp.cargarPersonaje(); 
+		 * gp.nuevoPersonaje(); 
+		 * gp.mostrarListaPesonajes();
+		 * gp.informacionPersonaje("DAN");
+		 */
+
+		gp.cargarPersonaje();
+
+		// gp.nuevoPersonaje();
+		// gp.mostrarListaPesonajes();
+
+		gp.informacionPersonaje("DAN");
 		
 		
 		
-		gp.mostrarListaPesonajes();
-		System.out.println("adfnsa");
+		//prueba parecida a la de la clase tienda
+		System.out.print("Nombre del Personaje: ");
+		String Nombre = sc.nextLine();
+		System.out.println("Datos acatuales.");
+		System.out.println(Nombre);
+		
+		
+		gp.modificarPersonaje(Nombre);
+		
+		
+		/*System.out.println("Introduce el nombre del personaje que quieres modificar");
+		String Nombre = sc.nextLine();
+		System.out.println("Datos acatuales.");
+		System.out.println(Nombre);
+		
+		gp.BuscarInfo(Nombre);
+		//quiero que aqui aparezcan los datos para que se puedan ir mirando y ver el valor anterior en caso de no querer cambiarlo
+		
+		System.out.println("Si algun dato no quieres cambiarlo vuelve a escribir el mismo valor");
+		System.out.println("Introduce el nuevo Nivel");
+		int Nivel = sc.nextInt();
+		System.out.println("Introduce el nueva Vida");
+		int Vida = sc.nextInt();
+		System.out.println("Introduce el nueva Fuerza");
+		int Fuerza = sc.nextInt();
+		System.out.println("Introduce el nueva Destreza");
+		int Destreza = sc.nextInt();
+		System.out.println("Introduce el nueva Constitucion");
+		int Constitucion = sc.nextInt();
+		System.out.println("Introduce el nueva Inteligencia");
+		int Inteligencia = sc.nextInt();
+		System.out.println("Introduce el nuevo Oro");
+		int Oro = sc.nextInt();
+		 
+		
+		
+
+		gp.modificarDatos(Nombre, Nivel, Vida, Fuerza, Destreza, Constitucion, Inteligencia, Oro);*/
+		
+	
+		
+		
+		
+
 	}
 
 }
